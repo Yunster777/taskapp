773a660461abeedcf2d843fc9beffcddfd2bac14
Yunster777 <kysung14@gmail.com>
2023-12-22 17:08:26 +0900

    ListView, Pagination, DetailView 까지(Ch1-7)

--- taskproject/taskapp/views.py
+++ taskproject/taskapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.core.paginator import Paginator
 
-from .models import Task
+from .models import Task, ChecklistItem
 
 
 def index(request):
@@ -14,9 +15,14 @@ class TaskListView(TemplateView):
     template_name = "pages/task_list.html"
 
     def get_context_data(self, **kwargs):
-        tasks = Task.objects.all()
+        tasks = Task.objects.filter(due__gte=timezone.now()).order_by("-due")
+        paginator = Paginator(tasks, 4)
+        page_number = self.request.GET.get("page", "1")
+        paging = paginator.get_page(page_number)
+
         return {
-            "tasks": tasks,
+            # "tasks": tasks,
+            "paging": paging,
         }
 
 
@@ -30,4 +36,16 @@ class TaskCreateView(CreateView):
 class TaskPreviousView(ListView):
     model = Task
     template_name = "pages/task_previous_list.html"
-    queryset = Task.objects.filter(due__lt=timezone.now())
+    queryset = Task.objects.filter(due__lt=timezone.now()).order_by("-due")
+    paginate_by = 4
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = "pages/task_detail_view.html"
+    pk_url_kwarg = "task_id"  # primary key
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["checklists"] = ChecklistItem.objects.filter(task=self.object).all()
+        return context
