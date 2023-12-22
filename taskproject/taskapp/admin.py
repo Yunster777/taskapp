afe2546f2feec357e391940c7f51b85b0631fe46
Yunster777 <kysung14@gmail.com>
2023-12-22 17:09:06 +0900

    ListView, Pagination, DetailView 까지(Ch1-7)

--- taskproject/taskapp/admin.py
+++ taskproject/taskapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, ChecklistItem
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "type", "due")
+    list_display_links = ("title",)
+
+
+class ChecklistItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "content", "checked")
+    list_display_links = ("content",)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(ChecklistItem, ChecklistItemAdmin)
--- taskproject/taskapp/models.py
+++ taskproject/taskapp/models.py
@@ -17,9 +17,15 @@ class Task(models.Model):
     due = models.DateTimeField(null=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
 
+    def __str__(self):
+        return self.title
+
 
 class ChecklistItem(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     content = models.CharField(max_length=100, null=False)
     checked = models.BooleanField(null=False, default=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False)
+
+    def __str__(self):
+        return self.content
